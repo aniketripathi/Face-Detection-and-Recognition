4467542ec0868f8d1e50fac88afafe3899e2a907
Aniket Kumar Tripathi <aniketkumartripathi@gmail.com>
2021-02-05 01:32:12 +0530

    #Fix scan() function in face_detection
    1. Fix scan() in face_detection
    2. Add visual studio project files to .gitignore
    3. Add location attribute to Face class

--- src/engine/Face.py
+++ src/engine/Face.py
@@ -7,9 +7,10 @@ Created on 18-Jan-2021
 
 class Face: 
 
-    def __init__(self, face_id, image_id, signature, recognized=False):
+    def __init__(self, face_id, image_id, location, signature, recognized=False):
         self.id = face_id
         self.image = image_id
+        self.location = location
         self.signature = signature
         self.recognized = recognized
         
--- src/engine/face_detection.py
+++ src/engine/face_detection.py
@@ -5,59 +5,38 @@ Created on Tue Feb  2 23:23:54 2021
 @author: Shashi
 """
 
-import cv2
 import face_recognition
 import os
-from datetime import datetime
-
-import Face
-import Image
+from .Face import Face
 
 
-class FaceDetection:
-    
-    def scan(self, images):
+
+def scan(images):
+
+    # Number of images
+    count = 0
+
+    for img in images:
+
+        # Image faceLocation stored in faceLoc
+        # face_location returns A list of tuples of found face locations in css
+        # (top, right, bottom, left) order
+        # count increment after successfully image scan
+        faceLoc = face.recognition.face_locations(img.imgdata())
+        count += 1
         
-		# Number of images
-		self.count = 0
-		
-		# Face_id
-		self.id = 0
-		
-        for img in self.images:
-            
-            # Image load in imgTest
-            self.imgTest = face_recognition.load_image_file(img)
-            
-            # Image faceLocation stored in faceLoc
-			# face_location returns A list of tuples of found face locations in css (top, right, bottom, left) order
-            self.faceLoc = face_recognition.face_locations(self.imgTest)
-			
-			# count increment after successfully image scan
-			self.count += 1
-            
-            # if face not found faceLoc will be empty
-            # len(faceLoc) == 0 that means face not found
-            if len(self.faceLoc) == 0:
-                continue
-            
-            # else
-			# face found in given image
-            
-            # face_encodings returns A list of 128-dimensional face encodings (one for each face in the image)
-            self.encodeImg = face_recognition.face_encodings(self.faceLoc)
-            
-            for i in len(self.faceLoc):
-				# face_id = id
-				Face.face_id = self.id
-				self.id += 1
-				
-				# signature = 128-dimension face encoding for each face in the image
-				Face.signature = self.encodeImg[i]
-            
-            # image_id = name of image file + current date and time
-            Face.image_id = f'{os.path.splitext(img)[0]}'
-            
-            
-            
-        
+        # if face not found faceLoc will be empty
+        # len(faceLoc) == 0 that means face not found
+        # else face found in given image
+        if len(faceLoc) == 0:
+            continue
+
+        # face_encodings returns A list of 128-dimensional face encodings
+        # (one for each face in the image)
+        encodeImg = face_recognition.face_encodings(faceLoc)
+
+        faces = [Face(i, img.id, faceLoc[i], encodeImg[i]) for i in range(len(faceLoc))]
+
+        img.faces = faces
+
+    return count
